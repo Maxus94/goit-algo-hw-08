21bca8714a4cd78c2bfb36a4a7bfd7b4a67b0cce
Maxim <kmaxusk@gmail.com>
2025-02-20 11:17:57 +0200

    corrected-task

--- task.py
+++ task.py
@@ -1,15 +1,25 @@
 import heapq
 
-cabels = [4, 10, 3, 5, 1, 1, 2, 2, 2, 1, 3, 7]
+cabels = [1, 1, 2, 3]
 heapq.heapify(cabels)
 print(cabels) 
 connected_cabels = []
+sum_cumulative = 0
+previous_cable = 0
+current_cable = 0
 
-for _ in range(0, len(cabels)):
-    connected_cabels.append(heapq.heappop(cabels))
+for i in range(0, len(cabels)):
+    previous_cable = current_cable + previous_cable
+    current_cable = heapq.heappop(cabels)
+    sum_current = previous_cable + current_cable
+    connected_cabels.append(current_cable)    
+    if i > 0:
+        sum_cumulative += sum_current                
+
 
 connected = ''
 for cabel in connected_cabels:   
     connected += str(cabel) + " + "
 
 print(connected[:-2:])
+print(sum_cumulative)        
